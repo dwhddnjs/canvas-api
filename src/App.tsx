a533aa0d7de1876a93e56282a69566c881def9a3
Jongwon <per1215@naver.com>
2022-08-26 00:05:30 +0900

    📦 리액트 쿼리 설치후 연결합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import List from './pages/List/List';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
-          <Route path="list" />
+          <Route path="/" element={<Home />} />
+          <Route path="/list/:id" element={<List />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
