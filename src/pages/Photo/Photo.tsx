074d1b266df8195fde4d79c0c6b034df73028c32
Jongwon <per1215@naver.com>
2022-08-29 21:53:22 +0900

    ✨ 우클릭 드래그 추가합니다

--- src/pages/Photo/Photo.tsx
+++ src/pages/Photo/Photo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -9,12 +9,97 @@ type stateTypes = {
 };
 
 const Photo = () => {
+  const canvasRef = useRef<any>(null);
+  const context = canvasRef.current?.getContext('2d');
+
   const { state } = useLocation() as stateTypes;
+
+  const [isDragging, setIsDragging] = useState(false);
+  const img = new Image();
+  img.src = state.photo;
+
+  useEffect(() => {
+    if (!canvasRef) return;
+
+    const context = canvasRef.current?.getContext('2d');
+
+    img.onload = () => {
+      context?.drawImage(
+        img,
+        0,
+        0,
+        canvasRef.current?.width,
+        canvasRef.current?.height,
+      );
+    };
+  }, [context]);
+
+  const startDragging = ({ nativeEvent }: any) => {
+    nativeEvent.preventDefault();
+    setIsDragging(true);
+    const { offsetX, offsetY, clientX, clientY } = nativeEvent;
+    const { offsetLeft, offsetTop } = context?.canvas;
+    const x = clientX - offsetLeft;
+    const y = clientY - offsetTop;
+
+    dragImgRotate(
+      img,
+      canvasRef.current?.width,
+      canvasRef.current?.height,
+      90,
+      1,
+    );
+    console.log('x: ', x);
+    console.log('y: ', y);
+
+    // context.current?.beginPath();
+    // context.current?.moveTo(offsetX, offsetY);
+    console.log('드래그 시작');
+  };
+
+  const dragImgRotate = (
+    img: any,
+    x: any,
+    y: any,
+    degrees: any,
+    scale: any,
+  ) => {
+    let w = img.width * scale;
+    let h = img.height * scale;
+    context.save();
+    context.translate(x, y);
+    context.rotate((degrees * Math.PI) / 180);
+    context.drawImage(img, -(w / 2), -(h / 2), w, h);
+    context.restore();
+  };
+
+  const finishDragging = () => {
+    context.current?.closePath();
+    setIsDragging(false);
+    console.log('드래그 끝');
+  };
+
+  // const draw = ({ nativeEvent }: any) => {
+  //   if (!isDragging) return;
+  //   const { offsetX, offsetY } = nativeEvent;
+  //   context.current?.lineTo(offsetX, offsetY);
+  //   console.log(
+  //     'context.current?.lineTo(offsetX, offsetY): ',
+  //     context.current?.lineTo(offsetX, offsetY),
+  //   );
+  // };
 
   return (
     <PhotoContainer>
       <PhotoImg>
-        <img src={state.photo} alt="" width={890} height={800} />
+        <canvas
+          ref={canvasRef}
+          width={890}
+          height={800}
+          onContextMenu={startDragging}
+          onMouseUp={finishDragging}
+          // onMouseMove={draw}
+        />
       </PhotoImg>
     </PhotoContainer>
   );
